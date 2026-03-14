aebfee4476748a96e136a0d6384b45631cbe366b
divya9786wok-eng <divya9786wok@gmail.com>
2026-03-14 15:25:31 +0530

    Update otp.model.js

--- backend/models/otp.model.js
+++ backend/models/otp.model.js
@@ -9,22 +9,35 @@ export default (sequelize) =>
         defaultValue: DataTypes.UUIDV4,
         primaryKey: true,
       },
+
       userId: {
         type: DataTypes.UUID,
         allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
       },
+
       code: {
         type: DataTypes.STRING(6),
         allowNull: false,
+        validate: {
+          isNumeric: true,
+          len: [6, 6],
+        },
       },
+
       type: {
         type: DataTypes.ENUM("password_reset"),
         defaultValue: "password_reset",
       },
+
       expiresAt: {
         type: DataTypes.DATE,
         allowNull: false,
       },
+
       used: {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
@@ -33,5 +46,10 @@ export default (sequelize) =>
     {
       tableName: "otps",
       timestamps: true,
+      indexes: [
+        {
+          fields: ["userId", "code"],
+        },
+      ],
     }
   );

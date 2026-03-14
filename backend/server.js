e0deaa93f034f390d3b1a5ef3ae8793de4b846d3
divya9786wok-eng <divya9786wok@gmail.com>
2026-03-14 12:49:54 +0530

    Made sql connection

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error("Database connection failed:", err);
+        return;
+    }
+    console.log("MySQL Connected");
+});
+
+
+app.get("/", (req, res) => {
+    res.send("Inventory API running");
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

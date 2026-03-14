cdd389e70692dc30ce3df5100e9c4cd9eca605a2
divya9786wok-eng <divya9786wok@gmail.com>
2026-03-14 15:08:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Warehouses from "./pages/Warehouses";
 import Categories from "./pages/Categories";
 import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
+import forgotPassword from "./pages/forgotPassword";
 
 export default function App() {
   return (
@@ -24,6 +25,7 @@ export default function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<AuthForms initialMode="login" />} />
           <Route path="/signup" element={<AuthForms initialMode="signup" />} />
+          <Route path="/forgot-password" element={<forgotPassword/>} />
           <Route
             path="/dashboard"
             element={
